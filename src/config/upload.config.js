bc6b9eba764ddd32e362423f7abaa8445ebbb733
pedro-mais-aurq <pedrolu690@gmail.com>
2026-08-15 21:23:17 -0300

    Remove acceptAttribute from upload config
    Removed acceptAttribute from upload configuration.

--- src/config/upload.config.js
+++ src/config/upload.config.js
@@ -1,7 +1,6 @@
 export const UPLOAD_CONFIG = Object.freeze({
   bucketName: "model-uploads",
   allowedExtensions: Object.freeze(["stl", "3mf", "obj"]),
-  acceptAttribute: ".stl,.3mf,.obj",
   maxFiles: 1,
   maxFileSizeBytes: 50_000_000
 });
