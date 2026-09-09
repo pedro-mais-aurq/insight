9438ea791414a5f9f6452c826802ee0527e04cff
pedro-mais-aurq <pedrolu690@gmail.com>
2026-09-09 14:35:00 -0300

    Update WhatsApp number in contact configuration

--- src/config/contact.config.js
+++ src/config/contact.config.js
@@ -1,7 +1,7 @@
 import { MANUFACTURING_UI_CONFIG } from "./manufacturing-ui.config.js";
 
 export const CONTACT_CONFIG = Object.freeze({
-  whatsappNumber: import.meta.env?.VITE_WHATSAPP_NUMBER ?? "",
+  whatsappNumber: 554396522539,
   referenceMaterial: MANUFACTURING_UI_CONFIG.referenceMaterial,
   referenceLayerHeightMm: MANUFACTURING_UI_CONFIG.referenceLayerHeightMm
 });
